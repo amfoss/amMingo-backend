d1a59610ce79886a0a2a2114ca6aac1ada20c885
Naveen <naveensrinivas282@gmail.com>
2026-03-10 20:28:47 +0530

    refactor(db schema): Handle bingo tiles and game better

--- app/db/models.py
+++ app/db/models.py
@@ -20,7 +20,7 @@ class User(Base):
     email = Column(String, unique=True, nullable=False)
     code = Column(String, unique=True, nullable=False)
 
-    game_user_bingo = relationship("GameUserBingo", back_populates="user")
+    game_user_bingo = relationship("Bingo", back_populates="user")
 
 
 class Game(Base):
@@ -30,44 +30,40 @@ class Game(Base):
     )
 
     id = Column(Integer, primary_key=True)
-
     host_id = Column(Integer, ForeignKey("user.id"), nullable=False)
     winner_id = Column(Integer, ForeignKey("user.id"), nullable=True)
-
     start_time = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     end_time = Column(DateTime, nullable=False)
-
     description = Column(Text, nullable=False)
     location = Column(String(255), nullable=False)
-
     code = Column(String(6), unique=True, nullable=False)
-
-    size = Column(Integer, nullable=False)
-
+    board_size = Column(Integer, nullable=False)
     qr_img = Column(Text, nullable=True)
 
-    game_user_bingos = relationship("GameUserBingo", back_populates="game")
+    game_user_bingo = relationship("Bingo", back_populates="game")
 
 
-class Bingo(Base):
-    __tablename__ = "bingo"
+class BingoTiles(Base):
+    __tablename__ = "bingo_tiles"
+
     id = Column(Integer, primary_key=True)
     row = Column(Integer, nullable=False)
     col = Column(Integer, nullable=False)
     bingo_char = Column(String, nullable=False)
-    image_link = Column(String, unique=True)
+    bingo_id = Column(Integer, ForeignKey("bingo.id"))
+    image_url = Column(String, unique=True)
 
-    game_user_bingo = relationship("GameUserBingo", back_populates="bingo")
+    game_user_bingo = relationship("Bingo", back_populates="bingo_tiles")
 
 
-class GameUserBingo(Base):
-    __tablename__ = "game_user_bingo"
+class Bingo(Base):
+    __tablename__ = "bingo"
 
-    game_id = Column(ForeignKey("game.id"), primary_key=True)
-    user_id = Column(ForeignKey("user.id"), primary_key=True)
-    bingo_id = Column(ForeignKey("bingo.id"), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    game_id = Column(Integer, ForeignKey("game.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
     points = Column(Integer, default=0)
 
     game = relationship("Game", back_populates="game_user_bingo")
     user = relationship("User", back_populates="game_user_bingo")
-    bingo = relationship("Bingo", back_populates="game_user_bingo")
+    bingo_tiles = relationship("BingoTiles", back_populates="game_user_bingo")
